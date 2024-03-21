c658117d2388f903f4f58a75011f04470531f17a
emil1abb <emil_abbasov_work@mail.ru>
2024-03-21 12:49:25 +0400

    changes

--- books/models.py
+++ books/models.py
@@ -8,3 +8,6 @@ class Book(models.Model):
     thumbnail_url = models.CharField(max_length=256, null=True)
     short_description = models.CharField(max_length=256, null=True)
     long_description = models.TextField(null=True)
+    
+    def __str__(self):
+        return self.title
